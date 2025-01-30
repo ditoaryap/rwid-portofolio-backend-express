2cfe49abdb59ddc12c1a410cdb9934279a09e528
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2025-01-31 00:00:27 +0700

    update env & index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+import "dotenv/config";
+
 import express from "express";
-const app = express()
-const port = process.env.PORT || 3000
-
-
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World</h1>")
-})
+  res.send("<h1>Hello World</h1>");
+});
 
-app.listen(port, ()=> {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
