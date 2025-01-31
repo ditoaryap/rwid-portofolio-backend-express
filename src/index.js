6bbf60cf63a95f1bc5a8507114e1e6eaf8fe5b60
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2025-01-31 19:42:38 +0700

    add post req

--- src/index.js
+++ src/index.js
@@ -4,20 +4,39 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 const users = [];
 
-app.post("/", (req, res) => {
+app.get("/user", (req, res) => {
   res.json({
-    id: 2,
-    name: "test",
+    users: users,
   });
 });
 
-app.get("/", (req, res) => {
+app.post("/user", (req, res) => {
+  const userInput = req.body;
+
+  users.push(userInput);
+
+  res.json({
+    user: userInput,
+  });
+});
+
+app.get("/user", (req, res) => {
   res.json({
     id: 1,
     name: "dito",
     users,
+  });
+});
+
+app.get("/user/:id", (req, res) => {
+  const id = req.id;
+  res.json({
+    id: id,
+    name: "dito",
   });
 });
 
