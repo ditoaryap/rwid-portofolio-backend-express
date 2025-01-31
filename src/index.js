7eb909aa9e30f2f2dae98f5313781a14cd9fa5fa
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2025-01-31 19:23:43 +0700

    add post

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+const users = [];
+
+app.post("/", (req, res) => {
+  res.json({
+    id: 2,
+    name: "test",
+  });
+});
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+  res.json({
+    id: 1,
+    name: "dito",
+    users,
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Running at http://127.0.0.1:${port}`);
 });
